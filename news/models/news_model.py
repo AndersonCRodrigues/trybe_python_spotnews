a6a5671602847276350c7e66fcaf924c72f9e310
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-08-29 20:14:34 -0300

    add data validator

--- news/models/news_model.py
+++ news/models/news_model.py
@@ -2,11 +2,24 @@ from django.db import models
 from news.models.user_model import Users
 from news.models.category_model import Categories
 from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+from datetime import datetime
 
 
 def validate_title(value):
     if len(value.split()) < 2:
         raise ValidationError(("O título deve conter mais de uma palavra."))
+
+
+def validate_date_format(value):
+    try:
+        datetime.strptime(str(value), "%Y-%m-%d")
+
+        return True
+    except ValueError as e:
+        raise ValidationError(
+            "Use o formato AAAA-MM-DD e uma data igual ou anterior a hoje."
+        ) from e
 
 
 class News(models.Model):
@@ -15,7 +28,7 @@ class News(models.Model):
     )
     content = models.TextField(null=False)
     author = models.ForeignKey(Users, on_delete=models.CASCADE)
-    created_at = models.DateField()
+    created_at = models.DateField(validators=[validate_date_format])
     image = models.ImageField(upload_to="img/", null=True, blank=True)
     categories = models.ManyToManyField(Categories)
 
