226efc2afba886c719e9ce9c6cd47e3b899ea9a6
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-08-29 20:30:45 -0300

    refact data validator

--- news/models/news_model.py
+++ news/models/news_model.py
@@ -10,14 +10,20 @@ def validate_title(value):
 
 def validate_date_format(value):
     try:
-        datetime.datetime.strptime(value, "%Y-%m-%d")
+        datetime.strptime(str(value), "%Y-%m-%d")
+
+        return True
     except ValueError:
-        raise ValidationError(("A data deve estar no formato AAAA-MM-DD."))
+        raise ValidationError(
+            "Use o formato AAAA-MM-DD."
+        )
 
 
 class News(models.Model):
-    title = models.CharField(max_length=200, validators=[validate_title])
-    content = models.TextField()
+    title = models.CharField(
+        max_length=200, null=False, validators=[validate_title]
+    )
+    content = models.TextField(null=False)
     author = models.ForeignKey("Users", on_delete=models.CASCADE)
     created_at = models.DateField(validators=[validate_date_format])
     image = models.ImageField(upload_to="img/", null=True, blank=True)
