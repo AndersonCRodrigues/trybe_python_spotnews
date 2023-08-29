245b23e21af480d8fc6c83cf2a07f5c497a754f3
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-08-29 20:22:06 -0300

    req 03

--- news/models/news_model.py
+++ news/models/news_model.py
@@ -1,8 +1,5 @@
 from django.db import models
-from news.models.user_model import Users
-from news.models.category_model import Categories
 from django.core.exceptions import ValidationError
-from django.utils.translation import gettext_lazy as _
 from datetime import datetime
 
 
@@ -13,24 +10,18 @@ def validate_title(value):
 
 def validate_date_format(value):
     try:
-        datetime.strptime(str(value), "%Y-%m-%d")
-
-        return True
-    except ValueError as e:
-        raise ValidationError(
-            "Use o formato AAAA-MM-DD e uma data igual ou anterior a hoje."
-        ) from e
+        datetime.datetime.strptime(value, "%Y-%m-%d")
+    except ValueError:
+        raise ValidationError(("A data deve estar no formato AAAA-MM-DD."))
 
 
 class News(models.Model):
-    title = models.CharField(
-        max_length=200, null=False, validators=[validate_title]
-    )
-    content = models.TextField(null=False)
-    author = models.ForeignKey(Users, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, validators=[validate_title])
+    content = models.TextField()
+    author = models.ForeignKey("Users", on_delete=models.CASCADE)
     created_at = models.DateField(validators=[validate_date_format])
     image = models.ImageField(upload_to="img/", null=True, blank=True)
-    categories = models.ManyToManyField(Categories)
+    categories = models.ManyToManyField("Categories")
 
     def __str__(self):
         return self.title
