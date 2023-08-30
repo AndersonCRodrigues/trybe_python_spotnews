db25b56fb41007e86fa561b002896a961f71ed9a
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-08-29 21:06:18 -0300

    add urls

--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from news.views import home
+from news.views import home, category_form, news_details
 
 urlpatterns = [
     path("", home, name="home-page"),
+    path('news/<int:id>/', news_details, name='news-details-page'),
+    path('categories/', category_form, name='categories-form'),
 ]
